8dc40131b86ac7a901866f0e48d607ac3df48d79
Christian Vondrak <christian20599@gmail.com>
2025-10-15 13:25:15 -0400

    se implemento la seccion

--- resources/js/pages/marketing/Home.vue
+++ resources/js/pages/marketing/Home.vue
@@ -2,6 +2,7 @@
 import HeroSection from '@/components/marketing/sections/HeroSection.vue'
 import ServicesSection from '@/components/marketing/sections/ServicesSection.vue';
 import DefaultLayout from '@/layouts/marketing/DefaultLayout.vue';
+import HowWeWorkSection from '@/components/marketing/sections/HowWeWorkSection.vue';
 import WorkSection from '@/components/marketing/sections/WorkSection.vue';
 import ContactSection from '@/components/marketing/sections/ContactSection.vue';
 </script>
@@ -11,6 +12,7 @@ import ContactSection from '@/components/marketing/sections/ContactSection.vue';
     <DefaultLayout>
       <HeroSection />
       <ServicesSection />
+      <HowWeWorkSection wide />
       <WorkSection />
       <ContactSection />
     </DefaultLayout>
